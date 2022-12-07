0d30b0b4429fda86beffa8624f516af11c259261
Lyubimov <rellitsabul@mail.ru>
2022-12-07 18:53:49 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        User user1 = new User ("Anastasiya", "Gribanova", (byte)21);
+        User user2 = new User ("Anastasiya2", "Gribanova2", (byte)21);
+        User user3 = new User ("Anastasiya3", "Gribanova3", (byte)21);
+        User user4 = new User ("Anastasiya4", "Gribanova4", (byte)21);
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
